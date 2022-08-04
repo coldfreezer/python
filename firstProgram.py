4bd25c382f690c3ede891017deb23bbf5c04697f
vikas_singh <92245748+coldfreezer@users.noreply.github.com>
2022-08-04 18:18:57 +0530

    Update firstProgram.py

--- firstProgram.py
+++ firstProgram.py
@@ -2,11 +2,11 @@ firstName = "Saksham"
 lastName = "Bhargava"
 age = 24.5
 Roll_number = "21MCAN001"
-isAdult = True
-isChild = False
+#isAdult = True  #not required
+#isChild = False #not required
 
 print(firstName,lastName)
-if(isAdult==True):
+if(age>=18):
     print("Yes, "+firstName,lastName+" is Adult")
 else:
     print("No, "+firstName,lastName+" is not Adult")
